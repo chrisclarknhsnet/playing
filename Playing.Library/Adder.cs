711c62f85b9be0c8f03f0d07ac191f3fb9fe5078
chrisclarknhsnet <52541599+chrisclarknhsnet@users.noreply.github.com>
2023-07-14 13:05:53 +0100

    Test of .NET actions

---
+++ Playing.Library/Adder.cs
@@ -0,0 +1,10 @@
+﻿namespace Playing.Library
+{
+    public class Adder
+    {
+        public int AddNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+    }
+}
---
+++ Playing.UnitTests/AdderTests.cs
@@ -0,0 +1,28 @@
+using Playing.Library;
+
+namespace Playing.UnitTests
+{
+    public class AdderTests
+    {
+        private readonly Adder _adder;
+
+        public AdderTests()
+        {
+            _adder = new Adder();
+        }
+
+        [Fact]
+        public void Adds_Two_Numbers()
+        {
+            // Arrange
+            var no1 = 4;
+            var no2 = 5;
+
+            // Act
+            var result = _adder.AddNumbers(no1, no2);
+
+            // Assert
+            Assert.Equal(9, result);
+        }
+    }
+}
